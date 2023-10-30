7eec4c405c39540bbe4c194ff871402418fd3db8
Daniel Haida <danilgajda22@gmail.com>
2023-10-30 13:43:26 +0200

    fix

--- router/index.js
+++ router/index.js
@@ -15,23 +15,42 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) {
-    console.error('Ошибка подключения к базе данных:', err)
+    console.error('Database connection error:', err)
   } else {
-    console.log('Подключено к базе данных MySQL')
+    console.log('Connected to MySQL database')
   }
 })
 
 router.get('/products', (req, res) => {
   db.query('SELECT * FROM products', (error, results) => {
     if (error) {
-      console.error('Ошибка выполнения запроса:', error)
+      console.error('Query execution error:', error)
       res
         .status(500)
-        .json({ error: 'Произошла ошибка при получении продуктов' })
+        .json({ error: 'An error occurred while fetching products' })
     } else {
       res.json(results)
     }
   })
+})
+router.get('/products/:id', (req, res) => {
+  const productId = req.params.id
+  db.query(
+    'SELECT * FROM products WHERE id = ?',
+    [productId],
+    (error, results) => {
+      if (error) {
+        console.error('Query execution error:', error)
+        res
+          .status(500)
+          .json({ error: 'An error occurred while fetching the product' })
+      } else if (results.length === 0) {
+        res.status(404).json({ error: 'Product not found' })
+      } else {
+        res.json(results[0])
+      }
+    }
+  )
 })
 
 module.exports = router
